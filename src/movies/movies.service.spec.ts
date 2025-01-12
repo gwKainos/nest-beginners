9db7a78447d3e428100dde1325e504d18d674ca5
Kainos <gwkainos@gmail.com>
2025-01-12 19:40:52 +0900

    Test: 테스트 코드 작성 - update -> delete로 변경

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -57,21 +57,22 @@ describe('MoviesService', () => {
     });
   });
 
-  describe('update', () => {
-    it('should update a movie', () => {
+  describe('deleteOne', () => {
+    it('deletes a movie', () => {
       service.create({
         title: 'Test Movie',
         genres: ['test'],
         year: 2000,
       });
-      service.update(1, { title: 'Updated Test' });
-      const movie = service.getOne(1);
-      expect(movie.title).toEqual('Updated Test');
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
     });
 
     it('should throw a NotFoundException', () => {
       try {
-        service.update(999, {});
+        service.deleteOne(999);
       } catch (e) {
         expect(e).toBeInstanceOf(NotFoundException);
       }
