444eaa022c50e24b55e87a86952e26c0e01ffb69
Kainos <gwkainos@gmail.com>
2025-01-12 23:29:33 +0900

    Feat: update 작성

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -25,5 +26,10 @@ export class MoviesController {
   @Delete(':id')
   remove(@Param('id') movieId: number): void {
     return this.moviesService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  update(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.moviesService.update(movieId, updateData);
   }
 }
