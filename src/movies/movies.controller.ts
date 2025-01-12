c9411c07aa46ad8e7fe94d3e70626636894395d3
Kainos <gwkainos@gmail.com>
2025-01-12 22:45:41 +0900

    Feat: create 작성

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto): Movie {
+    return this.moviesService.create(movieData);
+  }
+}
