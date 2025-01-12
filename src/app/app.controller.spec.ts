bc9eff7d2a0e1d5f7de652f7a40d8b36f30ac0fc
Kainos <gwkainos@gmail.com>
2025-01-12 23:48:30 +0900

    Test: 테스트 코드 작성

---
+++ src/app/app.controller.spec.ts
@@ -0,0 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let controller: AppController;
+  let service: AppService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [
+        {
+          provide: AppService, // AppService를 모듈에 제공
+          useValue: {
+            getHello: jest.fn().mockReturnValue('Hello World!'),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<AppController>(AppController);
+    service = module.get<AppService>(AppService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('getHello', () => {
+    it('should return "Hello World!"', () => {
+      expect(controller.getHello()).toBe('Hello World!');
+      expect(service.getHello).toHaveBeenCalled();
+    });
+  });
+});
