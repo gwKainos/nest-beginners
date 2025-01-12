c5116809c8ad4cd1a38a60d19b304a05c544a49b
Kainos <gwkainos@gmail.com>
2025-01-12 19:14:15 +0900

    Feat: create 작성

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Movie } from "./entities/movie.entity";
+import { CreateMovieDto } from "./dto/create-movie.dto";
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  private movies: Movie[] = [];
+
+
+  create(movieData: CreateMovieDto): Movie {
+    // 새 Movie 객체를 변수에 담는다
+    const newMovie: Movie = {
+      id: this.movies.length + 1,
+      ...movieData,
+    };
+
+    // 내부 배열에 넣고
+    this.movies.push(newMovie);
+
+    // 새로 생성된 객체 자체를 반환
+    return newMovie;
+  }
+}
