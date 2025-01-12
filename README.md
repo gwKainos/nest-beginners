2947573823f6415bbf2e9821c8da2c2776e4f6f0
Kainos <gwkainos@gmail.com>
2025-01-12 18:03:44 +0900

    TDD 기본 구조 생성 - movies

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ $ nest new
  : 프로젝트명
  : npm
 ```
+
+## TDD Start
+### 기본 구조 생성
+```shell
+$ nest g mo movies
+$ nest g co movies
+$ nest g s movies
+```
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app/app.service';
 import { AppController } from './app/app.controller';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [MoviesModule],
+  controllers: [AppController, MoviesController],
   providers: [AppService],
 })
 export class AppModule {}
