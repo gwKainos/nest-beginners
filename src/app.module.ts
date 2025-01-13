7020aed472d8a62b0901c7badf4d8beded96dbc0
Kainos <gwkainos@gmail.com>
2025-01-13 15:37:55 +0900

    Feat: prisma Service 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app/app.service';
 import { AppController } from './app/app.controller';
 import { MoviesModule } from './movies/movies.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [MoviesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
